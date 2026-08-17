c0ad064b1eb8b1b3186a63a1690b0faa1a825e69
isu.craz <social.isu.craz@gmail.com>
2026-08-17 15:18:54 +0530

    chore: initializing Spring Boot project with MySql database configurations

---
+++ src/test/java/isu_craz/schoollibraryapp/SchoolLibraryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package isu_craz.schoollibraryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLibraryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
